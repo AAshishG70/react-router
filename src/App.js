6d52eb0387b90d123ad3d419759d80855137832b
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-15 18:50:24 -0700

    component setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { BrouseRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./component/about.page";
+import Contact from "./component/contact.page";
+import Homepage from "./component/homepage.page";
+import ErrorPage from "./component/404.page";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />}></Route>
+          <Route path="home" element={<Homepage />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/component/about.page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const about = () => {
+  return <div> About Us Page </div>;
+};
+
+export default about;
